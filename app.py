d45d3a7011629d36100026580cb8edf625525536
Artem Myk <artemmykytysyn@gmail.com>
2022-07-06 20:11:16 +0300

    Add completion datetime as a field

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+from datetime import datetime
 
 from db_connection import get_collection
+
+
+def get_utc_time():
+    return datetime.utcnow().strftime("%d/%m/%Y %H:%M UTC")
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,22 +18,23 @@ class LevelStats(Resource):
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument("level", required=True, type=str, location='args')
-        parser.add_argument("player", required=True, type=str, location='args')
+        parser.add_argument("player_nickname", required=True, type=str, location='args')
         args = parser.parse_args()
 
         level_name = args["level"]
         collection = get_collection(level_name)
 
-        player_name = args["player"]
-        cursor = collection.find().sort("time") if player_name == "*" \
-            else collection.find({"player": player_name}).sort("time")
+        player_name = args["player_nickname"]
+        cursor = collection.find().sort("level_time") if player_name == "*" \
+            else collection.find({"player_nickname": player_name}).sort("level_time")
 
         arr = []
         for doc in cursor:
             arr.append(
                 {
-                    "player": doc["player"],
-                    "time": doc["time"]
+                    "player_nickname": doc["player_nickname"],
+                    "level_time": doc["level_time"],
+                    "completion_datetime": doc["completion_datetime"]
                 }
             )
 
@@ -36,8 +43,8 @@ class LevelStats(Resource):
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument("level", required=True, type=str, location='args')
-        parser.add_argument("player", required=True, type=str, location='args')
-        parser.add_argument("time", required=True, type=float, location='args')
+        parser.add_argument("player_nickname", required=True, type=str, location='args')
+        parser.add_argument("level_time", required=True, type=float, location='args')
         args = parser.parse_args()
 
         level_name = args["level"]
@@ -45,8 +52,9 @@ class LevelStats(Resource):
 
         collection.insert_one(
             {
-                "player": args["player"],
-                "time": args["time"]
+                "player_nickname": args["player_nickname"],
+                "level_time": args["level_time"],
+                "completion_datetime": get_utc_time()
             }
         )
 
