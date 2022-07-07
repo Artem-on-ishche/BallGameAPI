25ec285323fe888a790f89311ceefccebd663b6e
Artem Myk <artemmykytysyn@gmail.com>
2022-07-07 15:49:29 +0300

    Add an entries_count parameter to the GET request

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import json
+
+from bson import json_util
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from datetime import datetime
 
 from db_connection import get_collection
 
+
+# get
+# http://127.0.0.1:5000/level_scores?level=Level00&player_nickname=*&entries_count=0
+#
+# post
+# http://127.0.0.1:5000/level_scores?level=Level00&player_nickname=deez_nutser&level_time=19.01
 
 def get_utc_time():
     return datetime.utcnow().strftime("%d/%m/%Y %H:%M UTC")
@@ -19,6 +28,7 @@ class LevelStats(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument("level", required=True, type=str, location='args')
         parser.add_argument("player_nickname", required=True, type=str, location='args')
+        parser.add_argument("entries_count", required=True, type=int, location='args')
         args = parser.parse_args()
 
         level_name = args["level"]
@@ -28,8 +38,14 @@ class LevelStats(Resource):
         cursor = collection.find().sort("level_time") if player_name == "*" \
             else collection.find({"player_nickname": player_name}).sort("level_time")
 
+        entries_count = args["entries_count"]
+
         arr = []
+        count = 0
         for doc in cursor:
+            if 0 < entries_count == count:
+                break
+
             arr.append(
                 {
                     "player_nickname": doc["player_nickname"],
@@ -37,6 +53,8 @@ class LevelStats(Resource):
                     "completion_datetime": doc["completion_datetime"]
                 }
             )
+
+            count += 1
 
         return arr, 200
 
@@ -50,15 +68,13 @@ class LevelStats(Resource):
         level_name = args["level"]
         collection = get_collection(level_name)
 
-        collection.insert_one(
-            {
-                "player_nickname": args["player_nickname"],
-                "level_time": args["level_time"],
-                "completion_datetime": get_utc_time()
-            }
-        )
+        collection.insert_one({
+            "player_nickname": args["player_nickname"],
+            "level_time": args["level_time"],
+            "completion_datetime": get_utc_time()
+        })
 
-        return 200
+        return {}
 
 
 api.add_resource(LevelStats, '/level_scores')
